64a84fe92b492679f3647096dadbdeb7e05739bf
larutow <lucas.rutowski@gmail.com>
2020-12-23 12:57:53 -0600

    rebuilt/refreshed backend in ls-back

---
+++ ls-back/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.listen(port, () => {
+    console.log('hello world');
+    console.log(`Server is running on port: ${port}`);
+});
+
+//
+
+client.connect(err => {
+  const collection = client.db("LocalSource").collection("products");
+  // perform actions on the collection object
+  console.log('Successfully connected to mongoDB: db=LocalSource, collection=products');
+  client.close();
+});
