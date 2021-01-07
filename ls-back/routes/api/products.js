c470d26b19cf369a383babe0f4f678eef588d5ae
larutow <lucas.rutowski@gmail.com>
2021-01-07 00:25:51 -0600

    psuedocode in comments started to define operations necessary to properly upload inventory

--- ls-back/routes/api/products.js
+++ ls-back/routes/api/products.js
@@ -5,9 +5,29 @@ const db = require('../../db');
 
 require('dotenv').config.config();
 
+router.get("/searchproducts", async (req, res) => {
+    try{
+        // await client.connect();
+        const database = db.get().db("LocalSource");
+        const collection = database.collection("products-test");
+
+        await collection.findOne({ email: req.body.searchterm }).then(products => {
+            if (products){
+
+            }
+        })
+})
+
 router.post("/uploadinventory", async (req, res) => {
     //TODO: validate webtoken / is user in DB
     
+    //Etsy Example: https://www.etsy.com/shop/ShelbyPageCeramics
+    //Shopify Example (map collections manually): https://shopursa.com/
+
+    //Etsy flow:
+    //1 - extract last word in URL (shopname)
+    //2 - Etsy API request for shop inventory
+    //3 - for each item
     /*
     res.body.
     */
@@ -21,5 +41,15 @@ router.post("/uploadinventory", async (req, res) => {
     try{
         const database = db.get().db("LocalSource");
         const collection = database.collection("products-test")
+    } catch {
+
     }
+});
+
+router.put("/updateinventory", async (req, res) => {
+    
+})
+
+router.delete("/delist", async (req, res) => {
+    
 })
