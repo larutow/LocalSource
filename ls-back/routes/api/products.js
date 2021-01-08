3ed280d5fa05a402a2738a7801c372fa2a8fc957
larutow <lucas.rutowski@gmail.com>
2021-01-07 21:41:44 -0600

    preliminary adding inventory functionality/modeling complete on server side based upon etsy store link entry

--- ls-back/routes/api/products.js
+++ ls-back/routes/api/products.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const db = require('../../db');
+const axios = require ('axios').default;
 
-require('dotenv').config.config();
+require('dotenv').config();
 
 router.get("/searchproducts", async (req, res) => {
     try{
@@ -12,22 +13,116 @@ router.get("/searchproducts", async (req, res) => {
         const collection = database.collection("products-test");
 
         await collection.findOne({ email: req.body.searchterm }).then(products => {
-            if (products){
-
-            }
-        })
+            if (products){ }
+        });
+    }
+    catch{}
 })
 
 router.post("/uploadinventory", async (req, res) => {
-    //TODO: validate webtoken / is user in DB
-    
+    //pass in URL & jwt use JWT to find user
     //Etsy Example: https://www.etsy.com/shop/ShelbyPageCeramics
     //Shopify Example (map collections manually): https://shopursa.com/
 
-    //Etsy flow:
+    //identify shop/platform type (using URL ) - return 200 if not ETSY or Shopify
+    const submittedUrl = req.body.shopUrl;
+    
+    //check if etsy or shopify
+    //substring to remove https://www.
+    const removewww = submittedUrl.substring(submittedUrl.indexOf(".")+1);
+    //substring to remove .com/etc
+    let websitename = removewww.substring(0,removewww.indexOf("."));
+    //expected output = esty
+
+    //TODO use platform selection instead?
+    if(websitename === "etsy"){
+        //etsy store logic
+        //recieve last part of name (ShopId)
+        let shopName = submittedUrl.substring(submittedUrl.lastIndexOf("/")+1);
+        //make mongodb request to receive all products // an index would be good here but let's just do a complete collection scan
+        // try{
+
+        //     await collection.findOne({title})
+        // }
+        //
+        let product = {
+            productname: '',
+            category:'',
+            variants:[
+                {
+                    title: '',
+                    price: 0,
+                    description: '',
+                    img_url: '',
+                    origin_url: '',
+                    profile_id: ''
+                }
+                
+            ]
+        }
+        
+        const database = db.get().db("LocalSource");
+        let collection = database.collection("products");
+        const etsyKey = process.env.ETSY_API;
+        let i = 0;
+        let next_offset = null;
+        do{ 
+            axios.get(`https://openapi.etsy.com/v2/shops/${shopName}/listings/active?limit=100&offset=${i}&includes=Images&api_key=${etsyKey}`)
+            .then(async function(response){
+                //for each product in the inventory
+                for(const result of response.data.results){
+                    await collection.findOne({title: result.title}).then(function(foundproduct){
+                        if(foundproduct){
+                            //add this result as a variant to the foundproduct
+                        }else{
+                            //create a new product and insert if product is not found in the collection
+                            const newProduct = {
+                                productname: result.title,
+                                category: result.taxonomy_path[0],
+                                variants:[
+                                    {
+                                        title: 'default-product',
+                                        price: result.price,
+                                        description: result.description,
+                                        img_url: result.Images[0].url_570xN,
+                                        origin_url: result.url,
+                                        profile_id: 'Shelby-Test'
+                                    }
+                                ]
+                            }
+                            console.log(newProduct);   
+                        }
+                    });
+
+                }
+                next_offset = response.data.next_offset;
+            });
+            i++;
+        }
+        while(next_offset != null);
+        
+        
+    }
+
+
+
+    
+    //TODO: validate webtoken / is user in DB
+    //FindUser & extract ID - helper function
+    
+    
+    /*Etsy flow:
     //1 - extract last word in URL (shopname)
     //2 - Etsy API request for shop inventory
-    //3 - for each item
+        https://openapi.etsy.com/v2/shops/{ShopName}/listings/active?includes=Images&api_key=ri13xa0vjan8omqk3q0jia74
+        While()
+        Paginate inventory (for shops w more than 100 items)
+    //3 - foreach(etsy inventory item found)
+        db = LocalSource
+        collection = products-test
+        await collection.findOne({})
+        if res.data.results[i].title
+    */
     /*
     res.body.
     */
@@ -53,3 +148,5 @@ router.put("/updateinventory", async (req, res) => {
 router.delete("/delist", async (req, res) => {
     
 })
+
+module.exports = router;
